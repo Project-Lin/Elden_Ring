a45d82a995507918899bff9943f5fdf14e19d5f9
Project-Lin <s11201015@gmail.com>
2024-09-19 22:55:50 +0800

    EP. 1 Netcode For Gameobjects

---
+++ Assets/Script/WorldSaveGameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace PL
+{
+    public class WorldSaveGameManager : MonoBehaviour
+    {
+        public static WorldSaveGameManager instance;
+
+        [SerializeField] int worldSceneIndex = 1;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else 
+            {
+                Destroy(gameObject);
+            }
+
+        }
+
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public IEnumerator LoadNewGame()
+        {
+            AsyncOperation loadOparation = SceneManager.LoadSceneAsync(worldSceneIndex);
+
+            yield return null ;
+        }
+    }
+}
+
